2b1e2baff3b70c9e898f1a5c78122e1ff6ad9fea
Shrivathsan Sridhar <vathsansridhar@gmail.com>
2024-11-07 02:30:41 +0530

    Updates

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import os
+
 from vibestream import app
+
+try:
+    os.makedirs(os.path.join(app.root_path, app.config['UPLOADED_VIDEOS_DEST']))
+    os.makedirs(os.path.join(app.root_path, app.config['UPLOADED_THUMBS_DEST']))
+
+except Exception as e:
+    pass
 
 if __name__ == '__main__':
     app.run(debug = True)
